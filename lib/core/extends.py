d6a2f4e9981a5e9e07c51af16f7017484f6ae5e0
dong.w <dong.twain@gmail.com>
2023-08-02 21:14:39 +0800

    校验自定义请求头返回的类型

--- lib/core/extends.py
+++ lib/core/extends.py
@@ -1,4 +1,5 @@
 from lib.core import common
+from lib.core.log import logger
 
 def header(header = {}, data = None):
     script_list = common.get_value("extend_list")
@@ -8,6 +9,8 @@ def header(header = {}, data = None):
             header_dict = common.get_value("extendinfo_dict")[script].inject_headers(header, data)
             if header_dict and isinstance(header_dict, dict):
                 result.update(header_dict)
+            else:
+                logger.error('%s脚本返回的header错误' % script)
     return result
 
 def data(header = {}, data = None):
