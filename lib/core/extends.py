e868e32768d7e437eb721c624bead31f72df3f81
dong.w <dong.twain@gmail.com>
2023-08-02 21:12:58 +0800

    校验自定义请求头返回的类型

--- lib/core/extends.py
+++ lib/core/extends.py
@@ -6,7 +6,8 @@ def header(header = {}, data = None):
     if script_list:
         for script in script_list:
             header_dict = common.get_value("extendinfo_dict")[script].inject_headers(header, data)
-            result.update(header_dict)
+            if header_dict and isinstance(header_dict, dict):
+                result.update(header_dict)
     return result
 
 def data(header = {}, data = None):
