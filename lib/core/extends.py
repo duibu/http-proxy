e447d4473f1c0e1ebd2781457a54ebde593bc872
dong.w <wdong@yozosoft.com>
2023-08-11 17:01:10 +0800

    fix: handle extends error

--- lib/core/extends.py
+++ lib/core/extends.py
@@ -5,12 +5,17 @@ def header(header = {}, data = None):
     script_list = common.get_value("extend_list")
     result = {}
     if script_list:
-        for script in script_list:
-            header_dict = common.get_value("extendinfo_dict")[script].inject_headers(header, data)
-            if header_dict and isinstance(header_dict, dict):
-                result.update(header_dict)
-            else:
-                logger.error('%s脚本返回的header错误' % script)
+        try:
+            current_script = ''
+            for script in script_list:
+                current_script = script
+                header_dict = common.get_value("extendinfo_dict")[script].inject_headers(header, data)
+                if header_dict and isinstance(header_dict, dict):
+                    result.update(header_dict)
+                else:
+                    logger.error('%s脚本返回的header错误' % script)
+        except Exception as e:
+            logger.error('扩展脚本 [%s] 出现错误 -> [%s]' % (current_script, e))
     return result
 
 def data(header = {}, data = None):
