85c8a7cccdb7c5701b586959856d2c6d1e97f692
dong.w <wdong@yozosoft.com>
2023-08-16 10:22:50 +0800

    解析http请求头

--- lib/parse/http_package.py
+++ lib/parse/http_package.py
@@ -6,19 +6,21 @@ class HttpRequestPacket(object):
     def parse(self, data):
         header_split = data.find(b'\r\n')
         data_split = data.find(b'\r\n\r\n')
-    
+
+        data_str = data.decode()
+
         # 请求行 Request-Line
-        self.request_line = data[:header_split]
+        self.request_line = data_str[:header_split]
         # 请求行由method、request uri、version组成
         self.method, self.request_uri, self.version = self.request_line.split() 
         
         # 请求头域 Request Header Fields
-        self.request_header = data[header_split+2:data_split]
+        self.request_header = data_str[header_split+2:data_split]
         self.headers = {}
-        for header in self.request_header.split(b'\r\n'):
-            k, v = header.split(b': ')
+        for header in self.request_header.split('\r\n'):
+            k, v = header.split(': ')
             self.headers[k] = v
-        self.host = self.headers.get(b'Host')
+        self.host = self.headers.get('Host')
         
         # 请求数据
         self.request_data = data[data_split+4:]
